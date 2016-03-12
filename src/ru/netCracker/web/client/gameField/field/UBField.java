5b70133803de238b79127cdbda591b73d3a4b7ee
rewweRrr <rewweRrr@gmail.com>
2016-03-12 18:21:26 +0300

    Revert "add server logic"
    This reverts commit c26e17300679238f8c434a4efe398ecec7e06e34.
    
    # Conflicts:
    #	src/ru/netCracker/web/server/Cell.java

--- src/ru/netCracker/web/client/gameField/field/UBField.java
+++ src/ru/netCracker/web/client/gameField/field/UBField.java
@@ -1,16 +1,9 @@
 package ru.netCracker.web.client.gameField.field;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Image;
-import ru.netCracker.web.client.events.ClickCellEvent;
-import ru.netCracker.web.client.events.MinesWeeperEventBus;
-import ru.netCracker.web.client.logic.CurrentState;
 
 /**
  * Created by rewweRrr on 02.03.2016
@@ -21,24 +14,7 @@ public class UBField extends Composite {
 
     private static ubFieldUiBinder ourUiBinder = GWT.create(ubFieldUiBinder.class);
 
-    private int currentRow;
-    private int currentCol;
-
-    @UiField
-    Image img;
-
-    @SuppressWarnings("UnusedParameters")
-    @UiHandler("img")
-    void onClickImg(ClickEvent event) {
-        CurrentState.getInstance().setCurrentRow(currentRow);
-        CurrentState.getInstance().setCurrentColumn(currentCol);
-        MinesWeeperEventBus.get().fireEvent(new ClickCellEvent());
-    }
-
-    public UBField(int currentCol, int currentRow) {
-        this.currentRow = currentRow;
-        this.currentCol = currentCol;
+    public UBField() {
         initWidget(ourUiBinder.createAndBindUi(this));
     }
-
 }
--- src/ru/netCracker/web/server/Cell.java
+++ src/ru/netCracker/web/server/Cell.java
@@ -1,7 +1,7 @@
 package ru.netCracker.web.server;
 
 /**
- * Created by rewweRrr on 12.03.2016.
+ * Created by rewweRrr on 12.03.2016
  */
 public class Cell {
 
